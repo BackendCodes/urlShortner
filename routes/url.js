94bbad66e2c68192343b3f85222ac8837a64ed89
lordsainath <lordsainath8@gmail.com>
2025-12-04 19:10:56 +0530

    temp commit

--- routes/url.js
+++ routes/url.js
@@ -1,18 +1,21 @@
 const express = require("express");
-const generateUrl = require("../controllers/GenerateShortUrl");
-const RedirectUrl = require("../controllers/RedirectUrl");
-const getAnalytics = require("../controllers/GetAnalytics");
-const restrictToLoogedinUserOnly = require("../middlewares/checkauth");
-const generateDiscordUrl = require("../controllers/GenerateDiscordShortUrl");
-
 const urlRoutes = express.Router();
+const { generateUrl, generateDiscordUrl } = require("../controllers/generate");
+const RedirectUrl = require("../controllers/redirect");
+const { getAnalytics } = require("../controllers/analytics");
+const { restrictToLoogedinUserOnly } = require("../middlewares/auth");
 
+// POST routes
 urlRoutes.post("/", restrictToLoogedinUserOnly, generateUrl);
+urlRoutes.post("/discord", generateDiscordUrl);
 
-urlRoutes.post('/discord', generateDiscordUrl)
+urlRoutes.post('/discord/', generateDiscordUrl)
 
 urlRoutes.get("/:id", RedirectUrl);
 
 urlRoutes.get("/analytics/:url", restrictToLoogedinUserOnly, getAnalytics);
+
+// GET redirect (MUST BE LAST)
+urlRoutes.get("/:id", RedirectUrl);
 
 module.exports = urlRoutes;
